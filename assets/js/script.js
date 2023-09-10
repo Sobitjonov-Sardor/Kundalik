15bf540cc68f15a02736619c6cb654a166d08d3b
Sobitjonov-Sardor <140070595+Sobitjonov-Sardor@users.noreply.github.com>
2023-09-10 17:10:39 +0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,46 @@
-const adminKod = document.querySelector('.admin__kod'),
-    students = document.querySelector('.students'),
-    send = document.querySelector('.send'),
-    pinkod = document.querySelector('.pinkod');
-
-const test = () => {
-    console.log(1);
-};
-
-send.addEventListener('click', (e) => {
-    e.preventDefault();
-    test();
-
-
-    Admin()
+const adminKod = document.querySelector(".admin__kod"), 
+  students = document.querySelector(".students"), 
+  send = document.querySelector(".send"), 
+  close = document.querySelector(".close"), 
+  notThere = document.querySelector(".notThere"); 
+ 
+let kod = ""; 
+ 
+send.addEventListener("click", (e) => { 
+  e.preventDefault(); 
+  Admin(); 
+}); 
+ 
+function Admin() { 
+  if (kod === adminKod.value) { 
+    students.classList.remove("active"); 
+  } else { 
+    students.classList.add("active"); 
+  } 
+} 
+ 
+close.addEventListener("click", function () { 
+  students.classList.add("active"); 
+}); 
+ 
+const student = document.querySelectorAll(".student"); 
+ 
+student.forEach((stud) => { 
+  const plus = stud.querySelector(".plus"); 
+  const StudentName = stud.querySelector(".student__name"); 
+  const studentNumber = stud.querySelector(".student__number"); 
+  console.log(plus); 
+  let student = ` 
+    <div class="student__notThere"> 
+      <p class="student__number"> 
+        ${studentNumber.innerHTML} 
+      </p> 
+      <p class="student__name"> 
+        ${StudentName.innerHTML} 
+      </p> 
+    </div> 
+    `; 
+  plus.addEventListener("click", function () { 
+    notThere.insertAdjacentHTML("beforeend", student); 
+  }); 
 });
-
-function Admin() {
-    let kod = 2020;
-    if (pinkod == kod) {
-        students.classList.add('active');
-    } else {
-        students.classList.remove('active');
-    }
-}
-
-const minus = document.querySelectorAll('.minus'),
-    plus = document.querySelectorAll('.plus'),
-    amount = document.querySelectorAll('.amount');
-
